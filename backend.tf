97a5518e9b88d4caced6e3842b20e0ea3d60c9f3
shubhamdhiman2116 <shubhamdhiman200021@gmail.com>
2026-03-30 00:12:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "your-terraform-state-bucket"
+        bucket = "your-terraform-state-bucket-4"
         key = "rds/project/terraform.tfstate"
         region = "ap-south-1"
         dynamodb_table = "terraform-lock"
